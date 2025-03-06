7cdffa6c2a4ba38fefa100bb38d6e93a9d27e69a
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-07 00:06:56 +0530

    Update variables.tf

--- infra/modules/cognito/variables.tf
+++ infra/modules/cognito/variables.tf
@@ -2,17 +2,12 @@ variable "pool_name" {
   description = "The name of the Cognito User Pool"
   type        = string
 }
-variable "client_id" {
-  description = "The client ID of the Cognito App"
-  type        = string
-}
- 
-variable "region" {
-  description = "The AWS region"
-  type        = string
-}
- 
 variable "user_pool_id" {
-  description = "The User Pool ID"
+  description = "The ID of the Cognito User Pool"
+  type        = string
+}
+ 
+variable "client_name" {
+  description = "The name of the Cognito App Client"
   type        = string
 }
