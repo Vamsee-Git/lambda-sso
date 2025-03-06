cfd0b4d5e2568b562b2fa81cadb73ce917462437
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-07 00:04:21 +0530

    Update variables.tf

--- infra/modules/api_gateway/variables.tf
+++ infra/modules/api_gateway/variables.tf
@@ -7,3 +7,18 @@ variable "lambda_function_arn" {
   description = "The ARN of the Lambda function"
   type        = string
 }
+
+variable "client_id" {
+  description = "The client ID of the Cognito App"
+  type        = string
+}
+ 
+variable "region" {
+  description = "The AWS region"
+  type        = string
+}
+ 
+variable "user_pool_id" {
+  description = "The User Pool ID"
+  type        = string
+}
