03155b9f8734d6855b189aee2031f6654ab322d5
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-07 08:44:33 +0530

    Create variables.tf

---
+++ infra/modules/cognito_user_pool_client/variables.tf
@@ -0,0 +1,9 @@
+variable "user_pool_id" {
+  description = "The ID of the Cognito User Pool"
+  type        = string
+}
+ 
+variable "client_name" {
+  description = "The name of the Cognito App Client"
+  type        = string
+}
