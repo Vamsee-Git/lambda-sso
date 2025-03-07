0eb8ce87df4ef96f55549cce3acf8e30d5e964bd
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-07 08:40:16 +0530

    Update main.tf

--- infra/modules/cognito/main.tf
+++ infra/modules/cognito/main.tf
@@ -6,31 +6,11 @@ resource "aws_cognito_user_pool" "this" {
  
   username_attributes = ["email"]
 }
-resource "aws_cognito_user_pool_client" "this" {
-  name          = var.client_name
-  user_pool_id  = var.user_pool_id
-  allowed_oauth_flows    = ["code", "implicit"]
-  allowed_oauth_scopes   = ["openid", "email"]
-  allowed_oauth_flows_user_pool_client = true
-  supported_identity_providers = ["COGNITO"]
-  callback_urls = ["http://localhost:3000/callback"]
-  logout_urls   = ["https://localhost.com/logout"]
-  explicit_auth_flows = [
-    "ALLOW_REFRESH_TOKEN_AUTH",
-    "ALLOW_USER_SRP_AUTH",
-    "ALLOW_ADMIN_USER_PASSWORD_AUTH",
-    "ALLOW_USER_PASSWORD_AUTH"
-  ]
-}
-
+ 
 output "user_pool_id" {
   value = aws_cognito_user_pool.this.id
 }
  
 output "user_pool_arn" {
   value = aws_cognito_user_pool.this.arn
-}
- 
-output "client_id" {
-  value = aws_cognito_user_pool_client.this.id
 }
